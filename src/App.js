95accc2519e5e782bc3c6401033e7684eaa7542a
Drey Liba <libaandrey@gmail.com>
2021-02-17 13:51:40 +0800

    PONTEJO_ANDRIAN: implement architecture in store locator

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import "./App.scss";
-import "antd/dist/antd.css";
-import { Button } from "antd";
-import GoogleMaps from "./GoogleMap";
+import Main from "./layout/Main";
 
-function App() {
+const App = () => {
   return (
-    <div className="main-body">
-      <div className="body-1">
-        <div className="body-item-1">
-          <h1>Hello Wolrd</h1>
-          <Button type="primary">Primary Button</Button>
-        </div>
-      </div>
-      <div className="body-2">
-        <div className="body-item-2">
-          <h1>Hello Universe</h1>
-          <Button type="danger">Dashed Button</Button>
-        </div>
-      </div>
-      <div className="body-3">
-        <div className="body-item-3">
-          <h1>Hello Galaxy</h1>
-          <Button type="dashed">Link Button</Button>
-        </div>
-      </div>
-      <GoogleMaps />
+    <div className="App">
+      <Main></Main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/features/brand/Brand.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './brand.scss';
+
+class Brand extends Component {
+    render() {
+        return ( 
+            <div className='brand-component'>
+                <div className='header'>Store Locator</div>
+            </div>
+        );
+    }
+}
+
+export default Brand;
